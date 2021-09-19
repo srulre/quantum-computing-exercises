8dbb1ff46c00f4f38f1be6a269a7727749fd37ec
srulre <srulre@gmail.com>
2021-09-19 09:59:39 +0300

    Add solution to Ex. 2.1

---
+++ ex2.py
@@ -0,0 +1,61 @@
+import numpy as np
+import argparse
+import pathlib
+from pydantic import BaseModel, ValidationError
+from typing import Optional
+from qiskit import QuantumCircuit, transpile
+from qiskit.providers.aer import QasmSimulator
+from qiskit.quantum_info import Statevector
+from qiskit.visualization import plot_histogram
+
+
+class GHZConfigModel(BaseModel):
+    qubit_count: int = 2
+    output_path: Optional[str] = None
+
+
+def ghz_generator(n_qubits: int) -> QuantumCircuit:
+    # Create a Quantum Circuit with n qubits
+    circuit = QuantumCircuit(n_qubits)
+    # Add a H gate on qubit 0
+    circuit.h(0)
+    for qubit in range(1, n_qubits):
+        # Add a CX (CNOT) gate on corresponding control qubit and target qubit
+        circuit.cx(0, qubit)
+
+    return circuit
+
+
+if __name__ == '__main__':
+
+    ### Ex 2.1
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--config_path", type=str,
+                        help="JSON configuration path")
+    args = parser.parse_args()
+
+    # parse to pydantic
+    try:
+        ghz_config = GHZConfigModel.parse_file(args.config_path)
+    except ValidationError as e:
+        print(e)
+
+    # extract parameters
+    n_qubits = ghz_config.dict()["qubit_count"]
+    if not n_qubits:
+        n_qubits = 2  # default ghz state will be the first bell state
+    if n_qubits < 2:
+        raise ValueError("Qubit count must be larger than 1")
+
+    # process output path
+    output_path_str = ghz_config.dict()["output_path"]
+    if not output_path_str:
+        output_path = pathlib.Path.cwd() / 'output.png'  # default output path is the current working directory
+    else:
+        output_path = pathlib.Path(output_path_str)
+
+    # create the circuit object for the bell state generator
+    ghz_state_circuit = ghz_generator(n_qubits)
+    # draw in graph
+    ghz_state_circuit.draw(output='mpl',
+                           filename=str(output_path))
